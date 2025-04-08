0e462de4dacc1a94b00b403b0549eac595b9e98a
Nam Nguyen <nam.nguyen@saritasa.com>
2025-04-08 09:20:01 +0700

    Revert "Allowed all origins"
    This reverts commit 2b64190eb995a87d215cc735b6902cb343c57967.

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from misc import healthcheck
 from process import process_endpoint
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "PUT", "POST", "DELETE", "HEAD", "OPTIONS"],
-    allow_headers=["*"],
-)
-
 app.include_router(healthcheck.router)
 app.include_router(process_endpoint.router)
 
